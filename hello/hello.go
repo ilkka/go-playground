3bdb276b12e32210ead029dfeb2389d05f83a8b3
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2012-09-14 23:41:16 +0300

    Form handling code

--- hello/hello.go
+++ hello/hello.go
@@ -5,13 +5,15 @@ import (
 	"net/http"
 	"appengine"
 	"appengine/user"
+	"html/template"
 )
 
 func init() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", root)
+	http.HandleFunc("/sign", sign)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func root(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 	u := user.Current(c)
 	if u == nil {
@@ -24,5 +26,34 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusFound)
 		return
 	}
-	fmt.Fprintf(w, "Hello, %v!", u)
+	fmt.Fprintf(w, guestBookForm)
 }
+
+const guestBookForm = `
+<html>
+<body>
+	<form action="/sign" method="post">
+		<label for="content">Content:</label>
+		<textarea name="content" rows="3" cols="60"></textarea><br/>
+		<button type="submit">Sign</button>
+	</form>
+</body>
+</html>
+`
+
+func sign(w http.ResponseWriter, r *http.Request) {
+	err := signTemplate.Execute(w, r.FormValue("content"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+var signTemplate = template.Must(template.New("sign").Parse(signTemplateHtml))
+
+const signTemplateHtml = `
+<html>
+<body>
+	<p>You wrote: {{.}}</p>
+</body>
+</html>
+`
