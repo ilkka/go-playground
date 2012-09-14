1afce90a3fc03748559f7bc7134861aa79676f02
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2012-09-15 00:00:28 +0300

    Store greetings in datastore

--- hello/hello.go
+++ hello/hello.go
@@ -1,12 +1,19 @@
 package hello
 
 import (
-	"fmt"
 	"net/http"
 	"appengine"
 	"appengine/user"
+	"appengine/datastore"
 	"html/template"
+	"time"
 )
+
+type Greeting struct {
+	Author string
+	Content string
+	Timestamp time.Time
+}
 
 func init() {
 	http.HandleFunc("/", root)
@@ -26,12 +33,30 @@ func root(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusFound)
 		return
 	}
-	fmt.Fprintf(w, guestBookForm)
+	q := datastore.NewQuery("Greeting").Order("-Date").Limit(10)
+	greetings := make([]Greeting, 0, 10)
+	if _, err := q.GetAll(c, &greetings); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := guestBookTemplate.Execute(w, greetings); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)		
+	}
 }
 
-const guestBookForm = `
+var guestBookTemplate = template.Must(template.New("book").Parse(guestBookTemplateHtml))
+
+const guestBookTemplateHtml = `
 <html>
 <body>
+	{{range .}}
+		{{with .Author}}
+		<p><b>{{.}}</b> wrote:</p>
+		{{else}}
+		<p>Anonymous wrote:</p>
+		{{end}}
+		<p>{{.Content}}</p>
+	{{end}}
 	<form action="/sign" method="post">
 		<label for="content">Content:</label>
 		<textarea name="content" rows="3" cols="60"></textarea><br/>
@@ -42,18 +67,19 @@ const guestBookForm = `
 `
 
 func sign(w http.ResponseWriter, r *http.Request) {
-	err := signTemplate.Execute(w, r.FormValue("content"))
+	c := appengine.NewContext(r)
+	g := Greeting {
+		Content: r.FormValue("content"),
+		Timestamp: time.Now(),
+	}
+	if u := user.Current(c); u != nil {
+		g.Author = u.String()
+	}
+	_, err := datastore.Put(c, datastore.NewIncompleteKey(c, "Greeting", nil), &g)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+	} else {
+		http.Redirect(w, r, "/", http.StatusFound)
 	}
 }
 
-var signTemplate = template.Must(template.New("sign").Parse(signTemplateHtml))
-
-const signTemplateHtml = `
-<html>
-<body>
-	<p>You wrote: {{.}}</p>
-</body>
-</html>
-`
